5c586bb3a1fb04704d067ffbd5b183af788411a4
himanshu savita <himanshusavita@himanshus-MacBook-Air.local>
2019-11-04 09:21:51 +0530

    Added

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,22 +2,81 @@ import request from 'request'
 import rp from 'request-promise'
 
 export default (router)=>{
+
+      router.get('/update-token', (req, res)=>{
+       
+        var options = {
+            uri: "https://graph.facebook.com/v5.0/oauth/access_token",
+            qs: {
+                grant_type: "fb_exchange_token",          
+                client_id:"426940641303361",
+                client_secret: "9201de519aee8843c2b7fe94f7ad0a5a",
+                fb_exchange_token : "EAAGETN1D20EBAOwFM3Q30NyLBDRcMyZCybsvUhyipskzZAwMfBiYoN3H6KCUk18MRZBlzINVJmflJK4bDe9hH57GhZCtLz3YcdWZCRJcS4pLYWmG2zWLRN40qrRLjls6xGZCUgfUtOR3NHZCSWWGqmSZBCjX6HubHG2DAe8EQfzQZC9QAuf65bYxCgAYZBd7mSLYVLX97HILumnwZDZD"
+            },
+            json: true
+        };
+        // {
+        //     "key": {
+        //       "access_token": "EAAGETN1D20EBAAKG2AY4hepWIzNtZCf8TGZCmaAB0f6P1WB3ypyhUGTAZB046iaEYXG8KJS7IIuxlkKkhRSp2hUEgyr334AGAPiO1560kcUZCxPOMPBcgKqUEuJpU9SvVmO6DV3WB79VVUPtUS4WJgP90Ohecesk86RvYd7ZBbwZDZD",
+        //       "token_type": "bearer",
+        //       "expires_in": 5184000
+        //     }
+        //   }
+        rp(options)
+        .then(function (parsedBody) {
+            res.send({
+                key: parsedBody
+           })
+        })
+        .catch(function (err) {
+            res.send({
+                key: err
+           })
+        });
+
+      })
  
       router.get('/facebook-page', (req, res)=>{
-            // GET https://graph.facebook.com/1034103903466704?fields=access_token&access_token=EAAGETN1D20EBAJemWQhgpahrbJqYOuzKZCmufsAzw2d9ZBJWeHS1NK2oFnJTZByZBvOvjD2QHstVlCy8IisUG8UF1pRDOnDXheh3KbMXU5XLLglgdQMZAWXEnfPA9OtsN6Yx9c0mZActxJ4GEOqECOEV6ghfuFXpZA3AgkLocoZCkR8zXEHhMSC8uX7mx0bdyr59yGDItpx88wZDZD
+            
+            const posts = [
+                {
+                    text : "",
+                    url : "https://media.farandwide.com/4e/50/4e505d7c1c3a4f64917a70d40ee1385c.jpeg"
+                },
+                {
+                    text: "",
+                    url: "https://s4.scoopwhoop.com/anj/sw/51b1f5e3-6473-4aae-a2c6-81b6474cdd68.jpg"
+                }
+            ]
+    //     curl -i -X GET "https://graph.facebook.com/v5.0/oauth/access_token?  
+    // grant_type=fb_exchange_token&          
+    // client_id=426940641303361&
+    // client_secret=9201de519aee8843c2b7fe94f7ad0a5a&
+    // fb_exchange_token=EAAGETN1D20EBAOwFM3Q30NyLBDRcMyZCybsvUhyipskzZAwMfBiYoN3H6KCUk18MRZBlzINVJmflJK4bDe9hH57GhZCtLz3YcdWZCRJcS4pLYWmG2zWLRN40qrRLjls6xGZCUgfUtOR3NHZCSWWGqmSZBCjX6HubHG2DAe8EQfzQZC9QAuf65bYxCgAYZBd7mSLYVLX97HILumnwZDZD
 
+
+            //https://9gag.com/v1/group-posts/group/football
             // {
             //     "access_token": "EAAGETN1D20EBACSsZAErRNODDBKN9QpzLMLTkVUIlzNm9WtlqoAs8cZCo9vJfdSXlCqt61zHOAR5XMQ3B94i1a6mGc7voLDQTtNUZB3ZCrI97qNqbLapW2SkCAFsfkjxZAZBP3t7aS84vF4mMq1Xb4fx3ZBpHj3oOtlPjU5i3fG4QZDZD",
             //     "id": "1034103903466704"
             //   }
+            // curl -i -X POST \
+            // -d "message=Testing multi-photo post!" \
+            // -d "attached_media[0]={"media_fbid":"1002088839996"}" \
+            // -d "attached_media[1]={"media_fbid":"1002088840149"}" \
+            // -d "access_token=<access_token>" \
+            // -d "published=false" \
+            // -d "scheduled_publish_time=1512068400" \
+            // -d "unpublished_content_type=SCHEDULED" \
+            // "https://graph.facebook.com/me/feed"
             var options = {
                 method: 'POST',
                 uri: 'https://graph.facebook.com/v5.0/1034103903466704/photos',
                 body: {
                     published : 'true',
-                    caption: "WTF Life is",
-                    url: "https://img-9gag-fun.9cache.com/photo/ayoe39W_700bwp.webp",
-                    access_token: "EAAGETN1D20EBANjfgB8cqZAvJXyZBi7HDPZBbtlGQVi1l59LC5nUefgMyfEwDEHpvZCmqNQBmW3SoGpFKq1AKeq0fgcGw9hddeXjsZBo944El37eUP30CSUKJMAn8E40NdK58BfsZC6AgJaQLDN3Bh3bRqzI7F6QNHAS2sH5BZAZApS2cJYYiVMmT9MRad57LZAioShFJRM1wpgZDZD"
+                    caption: "😂🤣",
+                    url: "https://i.imgflip.com/3f4ivz.jpg",
+                    access_token: "EAAGETN1D20EBAAKG2AY4hepWIzNtZCf8TGZCmaAB0f6P1WB3ypyhUGTAZB046iaEYXG8KJS7IIuxlkKkhRSp2hUEgyr334AGAPiO1560kcUZCxPOMPBcgKqUEuJpU9SvVmO6DV3WB79VVUPtUS4WJgP90Ohecesk86RvYd7ZBbwZDZD",
                 },
                 json: true
             };
@@ -34,12 +93,6 @@ export default (router)=>{
                    })
                 });
       })
-
-    // //   {
-    // //     "access_token": "EAAGETN1D20EBAJGilLjevZCsI9QsO39BYvOBZBBHMb2BUcW5RnyxbDN3co2ZCadY8ia3ZCVxq9DJpTcEE8cL8y4AG59ZBtA7f6ZBdSD5ai9XURrR4cRwiYY5oE0YZACnyK3GAQpkNY26ViStDWZChQKNK8UZAuyfiP9h0IcF0RMzb9oQRAFxuiiLcYfeB2pxbh9q7ZAYiPfybWswZDZD",
-    // //     "id": "1034103903466704"
-    // //   }
-    //   router.post('/', )
 
       return router;
 }
